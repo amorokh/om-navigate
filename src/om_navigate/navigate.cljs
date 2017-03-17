c894236f8a52973d8194566515f4fb125dad137e
amorokh <ola.ottemalm@gmail.com>
2017-03-17 07:35:48 +0100

    ...

--- src/om_navigate/navigate.cljs
+++ src/om_navigate/navigate.cljs
@@ -13,21 +13,21 @@
      (.navigate navigation (name target) (clj->js params)))))
 
 (defn- navigator-factory
-  [navigator-class navigation props]
-  (js/React.createElement navigator-class
+  [navigator navigation props]
+  (js/React.createElement navigator
     #js {:navigation navigation
          :screenProps props}))
 
-(defn- create-screen-proxy [class]
+(defn- create-screen-proxy [screen]
   (om/ui
-    static field navigationOptions (.-navigationOptions class)
+    static field navigationOptions (.-navigationOptions screen)
     Object
     (render [this]
-      (let [class-factory (om/factory class)
-            screen-props  (.. this -props -screenProps)
-            navigation    (.. this -props -navigation)
-            props         (assoc screen-props :navigation navigation)]
-        (class-factory props)))))
+      (let [screen-factory (om/factory screen)
+            screen-props   (.. this -props -screenProps)
+            navigation     (.. this -props -navigation)
+            props          (assoc screen-props :navigation navigation)]
+        (screen-factory props)))))
 
 (defn- create-navigator-proxy [navigator]
   (om/ui
@@ -41,28 +41,28 @@
             props        (if props props screen-props)]                          
         (navigator-factory navigator navigation props)))))
 
-(defn- transform-route-config
-  [[name config]]
-  (if (.-navigator-proxy? (:screen config))
-    [name config]
-    [name (assoc config :screen (create-screen-proxy (:screen config)))]))
-  
-(defn- transform-route-configs
-  [route-configs]
-  (let [transformed (mapcat #(transform-route-config %) route-configs)]
-    (clj->js (apply hash-map transformed))))
+(defn- transform-routes
+  [routes]
+  (reduce-kv
+    #(assoc %1 %2
+       (let [{:keys [screen]} %3]
+         (if (.-navigator-proxy? screen) 
+           %3 
+           {:screen (create-screen-proxy screen)})))
+    {}
+    routes))
 
 (defn- extract-queries
-  [route-configs]
-  (let [screens (map (fn [[_ config]] (:screen config)) route-configs)]
-    (mapcat #(om/get-query %) screens)))
+  [routes]
+  (let [screens (map (fn [[_ route]] (:screen route)) routes)]
+    (mapcat om/get-query screens)))
 
 (defn- create-navigator
-  [route-configs factory]
-  (let [queries   (into [] (extract-queries route-configs))
-        navigator (factory (transform-route-configs route-configs))
+  [routes factory]
+  (let [queries   (into [] (extract-queries routes))
+        navigator (factory (clj->js (transform-routes routes)))
         proxy     (create-navigator-proxy navigator)]
-    (when (first queries)
+    (when (seq queries)
       (specify! proxy om/IQuery (query [this] queries)))
     proxy))
 
